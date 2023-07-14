9ea98a000f78f747af2839c40340be66acfb41b2
Muhammad Ali <133991759+sheikh-ali3@users.noreply.github.com>
2023-07-14 11:41:00 +0500

    oop 3.cpp

--- oop 3/oop 3.cpp
+++ oop 3/oop 3.cpp
@@ -3,58 +3,330 @@
 
 #include <iostream>
 # include <conio.h>
+#include <string>
 using namespace std;
 
-class NumDays
-{
-    float hours, days;
-public:
-    NumDays(float h)
-    {
-        hours = h;
-        days = 0;
-    }
+/*task 01*/
+//class NumDays
+//{
+//	float hours, days;
+//public:
+//	NumDays(float h)
+//	{
+//		hours = h;
+//		days = 0;
+//	}
+//
+//	float get_hours()
+//	{
+//		return hours;
+//	}
+//
+//	float get_days()
+//	{
+//		days = hours / 8;
+//		return days;
+//	}
+//
+//	void operator+(NumDays& obj)
+//	{
+//		cout << "the sum of hours of objects = " << hours + obj.hours << endl;
+//	}
+//
+//	void operator-(NumDays& obj)
+//	{
+//		cout << "the difference of hours of objects = " << hours - obj.hours << endl;
+//	}
+//
+//	NumDays& operator++()
+//	{
+//		++hours;
+//		days = hours / 8;
+//		return *this;
+//	}
+//
+//	NumDays& operator--()
+//	{
+//		--hours;
+//		days = hours / 8;
+//		return *this;
+//	}
+//
+//	NumDays& operator++(int)
+//	{
+//		NumDays temp(*this);
+//		++hours;
+//		days = hours / 8;
+//		return temp;
+//	}
+//
+//	NumDays& operator--(int)
+//	{
+//		NumDays temp(*this);
+//		--hours;
+//		days = hours / 8;
+//		return temp;
+//	}
+//
+//	~NumDays()
+//	{
+//
+//	}
+//};
 
-    void set_hours()
-    {
-        cout << "enter working hours : ";
-        cin >> hours;
-    }
+/*task 02*/
+//class ship
+//{
+//	string name, built_year;
+//
+//public:
+//	ship()
+//	{
+//		set_name();
+//		set_year();
+//	}
+//
+//	void set_name()
+//	{
+//		cout << "enter ship name : ";
+//		getline(cin, name);
+//	}
+//
+//	string get_name()
+//	{
+//		return name;
+//	}
+//
+//	void set_year()
+//	{
+//		cout << "enter built year : ";
+//		getline(cin, built_year);
+//	}
+//
+//	string get_year()
+//	{
+//		return built_year;
+//	}
+//
+//	virtual void print()
+//	{
+//		cout << "ship name = " << name << endl;
+//		cout << "built year = " << built_year << endl;
+//	}
+//
+//	~ship()
+//	{
+//
+//	}
+//};
+//class cruiseShip : public ship
+//{
+//	int passengers;
+//
+//public:
+//	cruiseShip()
+//	{
+//		set_passengers();
+//	}
+//
+//	void set_passengers()
+//	{
+//		cout << "enter passenger capacity : ";
+//		cin >> passengers;
+//	}
+//
+//	int get_passengers()
+//	{
+//		return passengers;
+//	}
+//
+//	void print()
+//	{
+//		cout << "ship name = " << get_name() << endl;
+//		cout << "ship passenger carrying capacity = " << passengers << endl;
+//	}
+//
+//	~cruiseShip()
+//	{
+//
+//	}
+//};
+//class cargoShip : public ship
+//{
+//	int capacity;
+//
+//public:
+//	cargoShip()
+//	{
+//		set_capacity();
+//	}
+//
+//	void set_capacity()
+//	{
+//		cout << "enter the load carrying capacity in tons : ";
+//		cin >> capacity;
+//	}
+//
+//	int get_capacity()
+//	{
+//		return capacity;
+//	}
+//
+//	void print()
+//	{
+//		cout << "ship name = " << get_name() << endl;
+//		cout << "ship's load capacity = " << get_capacity() << endl;
+//	}
+//
+//	~cargoShip()
+//	{
+//
+//	}
+//};
 
-    float get_hours()
-    {
-        return hours;
-    }
+/*task 03*/
+//class BasicShape
+//{
+//	double area;
+//	
+//public:
+//	void setArea(double a)
+//	{
+//		area = a;
+//	}
+//
+//	double getArea()
+//	{
+//		return area;
+//	}
+//
+//	virtual void calcArea() = 0;
+//};
+//class Circle : public BasicShape
+//{
+//	long int centerX, centerY;
+//	double radius;
+//	
+//public:
+//	Circle(long int x, long int y, double r)
+//	{
+//		centerX = x;
+//		centerY = y;
+//		radius = r;
+//		calcArea();
+//	}
+//
+//	long int getCenterX()
+//	{
+//		return centerX;
+//	}
+//
+//	long int getCenterY()
+//	{
+//		return centerY;
+//	}
+//
+//	void calcArea()
+//	{
+//		double area = 3.14 * radius * radius;
+//		setArea(area);
+//	}
+//
+//	~Circle()
+//	{
+//
+//	}
+//};
+//class Rectangle : public BasicShape
+//{
+//	long int width, length;
+//
+//public:
+//	Rectangle(long int w, long int l)
+//	{
+//		width = w;
+//		length = l;
+//		calcArea();
+//	}
+//
+//	long int getWidth()
+//	{
+//		return width;
+//	}
+//
+//	long int getLength()
+//	{
+//		return length;
+//	}
+//
+//	void calcArea()
+//	{
+//		double area = length * width;
+//		setArea(area);
+//	}
+//
+//	~Rectangle()
+//	{
+//
+//	}
+//};
 
-    float calc_days()
-    {
-        days = hours / 8;
-        return days;
-    }
-
-    void operator+(NumDays& obj)
-    {
-        cout << "the sum of hours of objects = " << hours + obj.hours << endl;
-    }
-
-    void operator-(NumDays& obj)
-    {
-        cout << "the difference of hours of objects = " << hours - obj.hours << endl;
-    }
-
-    void operator++(NumDays& obj)
-    {
-
-    }
-    ~NumDays()
-    {
-
-    }
-};
+/*task 04*/
+class 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	/*NumDays obj(8.0), obj2(18.0);
+
+	cout << "hours 1 = " << obj.get_hours() << endl;
+	cout << "days 1 = " << obj.get_days() << endl;
+
+	cout << "hours 2 = " << obj2.get_hours() << endl;
+	cout << "days 2 = " << obj2.get_days() << endl;
+
+	obj + obj2;
+	
+	obj - obj2;
+
+	obj++;
+	cout << "after post increment :\n";
+	cout << " hours = " << obj.get_hours() << endl;
+	cout << " days = " << obj.get_days() << endl;
+
+	++obj;
+	cout << "after pre increment :\n";
+	cout << " hours = " << obj.get_hours() << endl;
+	cout << " days = " << obj.get_days() << endl;
+
+	obj2--;
+	cout << "after post decrement :\n";
+	cout << " hours = " << obj2.get_hours() << endl;
+	cout << " days = " << obj2.get_days() << endl;
+
+	--obj2;
+	cout << "after pre decrement :\n";
+	cout << " hours = " << obj2.get_hours() << endl;
+	cout << " days = " << obj2.get_days() << endl;*/
+
+	/*ship* Ship[3];
+
+	Ship[0] = new ship;
+	Ship[1] = new cruiseShip;
+	Ship[2] = new cargoShip;
+
+	for (int i = 0; i < 3; i++)
+	{
+		Ship[i]->print();
+		delete Ship[i];
+	}*/
+
+	/*Circle obj(2,3,5);
+	cout << "area of circle = " << obj.getArea() << endl;
+
+	Rectangle obj2(1,7);
+	cout << "area of rectangle = " << obj.getArea() << endl;*/
+
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
